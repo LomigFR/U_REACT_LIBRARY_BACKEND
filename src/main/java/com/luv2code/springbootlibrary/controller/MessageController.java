20e9ebb58172d8adf339a87a6e67da0455183edf
guillaume collet <guillaume.collet@ac-rennes.fr>
2023-09-04 16:32:38 +0200

    Admin services - request response

--- src/main/java/com/luv2code/springbootlibrary/controller/MessageController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/MessageController.java
@@ -1,11 +1,13 @@
 package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.luv2code.springbootlibrary.service.MessageService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,15 +24,29 @@ public class MessageController {
     private MessageService messageService;
 
     @Autowired
-    public MessageController(MessageService messageService){
+    public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
     @PostMapping("/secure/add/message")
     public void postMessage(@RequestHeader(value = "Authorization") String token,
-                            @RequestBody Message messageRequest){
+                            @RequestBody Message messageRequest) {
 
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messageService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        messageService.putMessage(adminQuestionRequest, userEmail);
     }
 }
---
+++ src/main/java/com/luv2code/springbootlibrary/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,15 @@
+package com.luv2code.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+/**
+ * @author Guillaume COLLET
+ */
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
