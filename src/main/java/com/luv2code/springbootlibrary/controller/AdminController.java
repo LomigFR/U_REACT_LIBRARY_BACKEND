6451cbedcd6d9898ad0c4878c060fada25d84742
guillaume collet <guillaume.collet@ac-rennes.fr>
2023-09-07 17:50:09 +0200

    Book quantity management admin feature

--- src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -5,16 +5,18 @@ import com.luv2code.springbootlibrary.service.AdminService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Guillaume COLLET
  */
-
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/admin")
@@ -27,6 +29,29 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
@@ -34,5 +59,18 @@ public class AdminController {
         if (admin == null || !admin.equals("admin")) {
             throw new Exception("Administration page only.");
         }
+
+        adminService.postBook(addBookRequest);
+    }
+
+    @DeleteMapping("/secure/delete/book")
+    public void deleteBook(@RequestHeader(value = "Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only.");
+        }
+
+        adminService.deleteBook(bookId);
     }
 }
--- src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +16,8 @@ public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEMail, Long bookid);
 
     List<Checkout> findBooksByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Checkout where book_id in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
